a3cf3bfac2471763fd93ce663ff6bb05b4849f7d
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2021-11-22 20:07:17 +0100

    Mas ejercicios hechos

--- js/regexp.js
+++ js/regexp.js
@@ -11,6 +11,10 @@ window.onload=iniciar
 
 function iniciar(){
     document.getElementById('iIP').onblur=comprobar
+    document.getElementById('matriculamoderna').onblur=comprobar
+    document.getElementById('matriculaantigua').onblur=comprobar
+    document.getElementById('dni').onblur=comprobar
+    document.getElementById('fecha').onblur=comprobar
 }
 
 function comprobar(){
@@ -19,5 +23,25 @@ function comprobar(){
     let exp1=new RegExp(/^(\d{1,3}\.){3}\d{1,3}/)
     if(!document.getElementById('iIP').value.match(exp1)){
         console.log('IP Errónea');
+    }
+    //n1
+    let expmatriculamoderna=new RegExp(/^(\d){4}[A-Z]{3}/)
+    if(!document.getElementById('matriculamoderna').value.match(expmatriculamoderna)){
+        console.log('Matricula moderna errónea');
+    }
+    //n2
+    let expmatriculaantigua=new RegExp(/[A-Z]{2}\d{4}[A-Z]{1,2}/)
+    if(!document.getElementById('matriculaantigua').value.match(expmatriculaantigua)){
+        console.log('Matricula antigua errónea');
+    }
+    //n3
+    let expdni=new RegExp()
+    if(!document.getElementById('dni').value.match(expdni)){
+        console.log('DNI erróneo');
+    }
+    //n4
+    let expfecha=new RegExp(/^(0?[1-9]|[12][0-9]|3[01])[/](0?[1-9]|1[012])[/\/](19|20)\d{2}$/)
+    if(document.getElementById('fecha').value.match(expfecha)){
+        console.log('Fecha errónea');
     }
 }
